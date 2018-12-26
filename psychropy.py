3dc33229deb12624ec805e9b4deaf54b6de86137
Alexandre Vaudrey <alexandre.vaudrey@gmail.com>
2018-12-26 18:35:27 +0100

    New properties for entering the state of moist air from couples of parameters RT and TR.

--- psychropy.py
+++ psychropy.py
@@ -26,6 +26,7 @@ Author: alexandre.vaudrey@gmail.com
 
 # Properties of humid air
 from CoolProp.CoolProp import HAPropsSI
+import sys
 # Value of one atmosphere
 ATM = 1.01325
 
@@ -36,6 +37,17 @@ ATM = 1.01325
 # specific entropy = S
 # Wet bulb temperature = B
 # Dew point temperature = D
+
+# Dictionnary of properties
+properties = {'R':'relative_humidity',
+              'W':'specific_humidity',
+              'T':'temperature',
+              'B':'wet_bulb_temperature',
+              'D':'dew_point_temperature',
+              'H':'specific_enthalpy',
+              'P':'pressure'}
+
+single_parameter_set_message = "At least two complementary properties of moist air are required to unambiguously identify the latter, any sole parameter can then not be used for so."
 
 class MoistAir(object):
     """
@@ -65,13 +77,73 @@ class MoistAir(object):
         # Relative humidity
         self.relative_humidity = 0.5
         # Specific humidity
-        self.specific_humidity = HAPropsSI('W',
-                                           'P', self.pressure*1e5,
-                                           'T', self.temperature+273.15,
-                                           'R', self.relative_humidity)
-    # TODO : create a function that can read the name of any attribute entered
-    # as PWT, PRT, BTR,... and so on, identify each parameter from a dictionary,
-    # and attribute the joined value to the corresponding parameter.
+        self._specific_humidity = HAPropsSI('W',
+                                            'P', self.pressure*1e5,
+                                            'T', self.temperature+273.15,
+                                            'R', self.relative_humidity)
+    # At least two values being required to identify unambiguously the state of
+    # a given moist air, any new moist air must be entered thanks to a couple of
+    # properties, as e.g. the dry and the wet bulb temperature or the dry
+    # temperature and the relative humidity. Both single property of moist air
+    # can then be get but not set directly.
+    @property
+    def specific_humidity(self):
+        """
+        Specific humidity, aka humidity ratio, defined as the ratio of the mass
+        of water vapour on the dry air one it is mixed with. Dimensionless.
+        """
+        return self._specific_humidity
+    @specific_humidity.setter
+    def specific_humidity(self, value):
+        print(single_parameter_set_message)
+        pass
+    # Dry temperature and relative humidity as entered parameters
+    @property
+    def TR(self):
+        """
+        Dry temperature and relative humidity as entered properties of the moist
+        air.
+        """
+        return self.temperature, self.relative_humidity
+    @TR.setter
+    def TR(self, values):
+        """
+        Dry temperature and relative humidity as known properties of moist air.
+        """
+        # Set of the temperature value
+        self.temperature = values[0]
+        # Check if the entered value of relative humidity is between 0 and 1
+        if (values[1] < 0):
+            self.relative_humidity = 0.0
+            print('Relative humidity is a number between 0% and 100%!')
+        elif (values[1] > 1):
+            self.relative_humidity = 1.0
+            print('Relative humidity is a number between 0% and 100%!')
+        else:
+            self.relative_humidity = values[1]
+        # Calculation of the corresponding specific humidity value
+        self._specific_humidity = HAPropsSI('W',
+                                            'P', self.pressure*1e5,
+                                            'T', self.temperature+273.15,
+                                            'R', self.relative_humidity)
+        pass
+    @property
+    def RT(self):
+        """
+        Relative humidity and dry temperature as entered properties of the moist
+        air.
+        """
+        return self.relative_humidity, self.temperature
+    @RT.setter
+    def RT(self, values):
+        """
+        Relative humidity and dry temperature as entered properties of the moist
+        air.
+        """
+        self.TR = values[::-1]
+        pass
 
 if __name__ == '__main__':
     inside = MoistAir()
+    inside.TR = 0.0, 0.8
+    print(inside.specific_humidity)
